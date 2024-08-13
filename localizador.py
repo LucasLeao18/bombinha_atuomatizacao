8d249476fb5c79baf6b6e33d58f58231150f8df1
Lucas <lucasleaopessoal@gmail.com>
2024-08-13 17:15:08 -0300

    Deletado Arquivos desncessarios

--- localizador.py
+++
@@ -1,9 +0,0 @@
-from pynput import mouse
-
-def on_click(x, y, button, pressed):
-    if pressed:
-        print(f'Posição do mouse: x={x}, y={y}')
-
-# Configura o listener para capturar os cliques do mouse
-with mouse.Listener(on_click=on_click) as listener:
-    listener.join()
